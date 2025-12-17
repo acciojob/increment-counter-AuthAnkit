003dbd95418a107025cc4a41475443682549299a
Ankit singh <167414480+AuthAnkit@users.noreply.github.com>
2025-12-17 21:41:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your JS code here. If required.
+let counter = document.getElementById("counter");
+let incrementBtn = document.getElementById("incrementBtn");
+let count = 0;
+incrementBtn.addEventListener("click", function () {
+	alert(count); 
+	count++;               
+	counter.innerText = count; 
+});
